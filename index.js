c1038ed54a413d8725f867ded1412a54d8bb0036
natsilRain <156608986+natsilRain@users.noreply.github.com>
2026-09-23 20:56:53 +0530

    Added new book functionality

--- index.js
+++ index.js
@@ -22,29 +22,35 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 function displayBooks() {
     myLibrary.forEach((book) => {
         const bookItem = document.createElement("div");
 
-        const title = document.createElement("h2");
-        const author = document.createElement("p");
-        const pages = document.createElement("p");
-        const read = document.createElement("p");
+        const bookTitle = document.createElement("h2");
+        const bookAuthor = document.createElement("p");
+        const bookPages = document.createElement("p");
+        const bookRead = document.createElement("p");
 
-        title.textContent = `${book.title}`;
-        author.textContent = `by ${book.author}`;
-        pages.textContent = `page count: ${book.pages}`;
-        read.textContent = `${book.read}`;
+        bookTitle.textContent = `${book.title}`;
+        bookAuthor.textContent = `by ${book.author}`;
+        bookPages.textContent = `page count: ${book.pages}`;
+        bookRead.textContent = `${book.read}`;
 
-        title.classList.add("title");
-        author.classList.add("author");
-        pages.classList.add("pages");
-        read.classList.add("status");
+        bookTitle.classList.add("title");
+        bookAuthor.classList.add("author");
+        bookPages.classList.add("pages");
+        bookRead.classList.add("status");
 
-        bookItem.appendChild(title);
-        bookItem.appendChild(author);
-        bookItem.appendChild(pages);
-        bookItem.appendChild(read);
+        bookItem.appendChild(bookTitle);
+        bookItem.appendChild(bookAuthor);
+        bookItem.appendChild(bookPages);
+        bookItem.appendChild(bookRead);
 
         bookItem.classList.add("card")
 
@@ -57,5 +63,33 @@ addBookToLibrary("The Great Gatsby", "E. Scott. Fitzgerald", 140, "reading");
 addBookToLibrary("Heart of Darkness", "Joseph Conrad", 101, "unread");
 addBookToLibrary("Treasure Island", "Robert Louis Stevenson", 225, "unread");
 addBookToLibrary("The Aeneid", "Virgil", 422, "unread");
+
+function processNewBook() {
+    const newBookTitle = document.querySelector(".bookTitle");
+    const newBookAuthor = document.querySelector(".bookAuthor");
+    const newBookPages = document.querySelector(".bookPages");
+    const newBookStatus = document.querySelector(".status");
+
+    newTitle = newBookTitle.value;
+    newAuthor = newBookAuthor.value;
+    newPages = newBookPages.value;
+    newStatus = newBookStatus.textContent;
+
+    addBookToLibrary(newTitle, newAuthor, newPages, newStatus);
+}
+
+const newBookDialog = document.querySelector(".new-book-dialog")
+const newBookBtn = document.querySelector(".newBookBtn");
+
+newBookBtn.addEventListener("click", (e) => {
+    processNewBook();
+    myLibrary.forEach((book) => {
+        const currentDiv = document.querySelector(".libraryDisplay");
+        removeAllChildNodes(currentDiv);
+    });
+    displayBooks();
+
+    newBookDialog.close();
+});
 
 displayBooks();
