1187d767042175e0ba82a4dbabd443562730d859
natsilRain <156608986+natsilRain@users.noreply.github.com>
2026-09-23 18:22:14 +0530

    Create display function

--- index.js
+++ index.js
@@ -20,23 +20,33 @@ function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
 
     myLibrary.push(newBook);
-    console.log("New book added to library!");
 }
 
 function displayBooks() {
     myLibrary.forEach((book) => {
-        const newDiv = document.createElement("div");
+        const bookItem = document.createElement("div");
 
-        const newContent = document.createTextNode((book.info()));
+        const title = document.createElement("h2");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const read = document.createElement("p");
 
-        newDiv.appendChild(newContent);
+        title.textContent = `${book.title}`;
+        author.textContent = `${book.author}`;
+        pages.textContent = `${book.pages}`;
+        read.textContent = `${book.read}`;
+
+        bookItem.appendChild(title);
+        bookItem.appendChild(author);
+        bookItem.appendChild(pages);
+        bookItem.appendChild(read);
 
         const currentDiv = document.querySelector(".libraryDisplay");
-        currentDiv.appendChild(newDiv);
-    })
+        currentDiv.appendChild(bookItem);
+    });
 }
 
-addBookToLibrary("The Great Gatsby", "F. Scott. Fitzgerald", 140, "reading");
+addBookToLibrary("The Great Gatsby", "E. Scott. Fitzgerald", 140, "reading");
 addBookToLibrary("Heart of Darkness", "Joseph Conrad", 101, "not read yet");
 addBookToLibrary("Treasure Island", "Robert Louis Stevenson", 225, "not read yet");
 addBookToLibrary("The Aeneid", "Virgil", 422, "not read yet");
